93cd7d1914dba5ee5577fe265886e194188ecb9e
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2021-10-02 16:26:41 +0700

    Update animation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import SVG, { XSvg } from "../../assets/svg";
+import SVG, {
+  Archive,
+  Event,
+  Instagram,
+  Web,
+  XSvg,
+  YearBook,
+} from "../../assets/svg";
 import Grain from "../../assets/grain2.png";
 import ReactTypingEffect from "react-typing-effect";
 
@@ -18,8 +25,8 @@ export default function Home() {
 }
 
 export function Sect1() {
-  const [textClass, setTextClass] = useState(" opacity-0");
-  const [headClass, setHeadClass] = useState(" opacity-0");
+  // const [textClass, setTextClass] = useState(" opacity-0");
+  // const [headClass, setHeadClass] = useState(" opacity-0");
   const [loadClass, setLoadClass] = useState(" ");
   const [grainEff, setGrainEff] = useState(" ");
   const [image, setImage] = useState(false);
@@ -28,12 +35,12 @@ export function Sect1() {
   const desc = [
     "FREKUENSI 27",
     "MOKLET'S FREQUENCY",
-    "THE LEGEND",
     "FREKUENSI 27",
+    "THE LEGEND",
   ];
 
   const blobs = (
-    <>
+    <div>
       <img
         draggable="false"
         src={Grain}
@@ -67,19 +74,19 @@ export function Sect1() {
         alt=""
         className="z-10 absolute top-6 left-3 transform rotate-180 scale-75"
       />
-    </>
+    </div>
   );
 
   const xAround = (
-    <>
-      <XSvg classes="z-10 absolute top-48 left-1/3 w-3 h-3" />
-      <XSvg classes="z-10 hidden md:block absolute top-24 left-1/5 w-3 h-3" />
-      <XSvg classes="z-10 hidden md:block absolute top-2/5 left-36 w-3 h-3" />
-      <XSvg classes="z-10 absolute bottom-1/4 left-1/3 w-3 h-3" />
-      <XSvg classes="z-10 absolute bottom-36 right-1/4 w-3 h-3" />
-      <XSvg classes="z-10 absolute bottom-1/2 right-1/5 w-3 h-3" />
-      <XSvg classes="z-10 absolute top-1/5 right-1/3 w-3 h-3 " />
-    </>
+    <div>
+      <XSvg className=" top-48 left-1/3 w-3 h-3 " />
+      <XSvg className=" hidden md:block top-24 left-1/5 w-3 h-3 " />
+      <XSvg className=" hidden md:block top-2/5 left-36 w-3 h-3 " />
+      <XSvg className=" bottom-1/4 left-1/3 w-3 h-3 " />
+      <XSvg className=" bottom-36 right-1/4 w-3 h-3 " />
+      <XSvg className=" bottom-1/2 right-1/5 w-3 h-3 " />
+      <XSvg className=" top-1/5 right-1/3 w-3 h-3  " />
+    </div>
   );
 
   useEffect(() => {
@@ -89,12 +96,12 @@ export function Sect1() {
         setLoadClass(" opacity-0");
       }, 500);
       setTimeout(() => {
-        setHeadClass(" animate-blink");
+        // setHeadClass(" animate-blink");
         setLoading(false);
       }, 800);
-      setTimeout(() => {
-        setTextClass(" animate-blink");
-      }, 2000);
+      // setTimeout(() => {
+      //   setTextClass(" animate-blink");
+      // }, 2000);
     }
     setGrainEff("animate-grain");
   }, [image]);
@@ -129,9 +136,11 @@ export function Sect1() {
     <section id="sec0" className="sec-base relative overflow-hidden">
       {loading && load}
       <div className="flex flex-col items-center justify-center h-screen">
-        <p className={"text-white h1 mb-6 z-10" + headClass}>WE ARE</p>
+        {/* <p className={"text-white h1 mb-6 z-10" + headClass}>WE ARE</p>  */}
+        <p className="text-white h1 mb-6 z-10">WE ARE</p>
         <Logo done={image} />
-        <p className={"text-white h1 mt-8 tracking-widest z-10 " + textClass}>
+        {/* <p className={"text-white h1 mt-8 tracking-widest z-10 " + textClass}>   */}
+        <p className="text-white h1 mt-8 tracking-widest z-10 mb-20 md:mb-12">
           <ReactTypingEffect
             speed={100}
             eraseSpeed={100}
@@ -146,13 +155,43 @@ export function Sect1() {
           />
         </p>
       </div>
-      <div className="z-10 hidden md:block absolute inset-y-1/2 right-0 px-12 py-3">
-        <p className="text-white text-lg font-work ">F</p>
+      <div className="z-10 flex md:flex-col justify-center items-center absolute inset-x-1/2 bottom-16 md:-right-1/3 md:inset-y-1/2 md:my-3 ">
+        <a
+          href="https://instagram.com/frekuensi27_"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative p-1 transition duration-100 hover:bg-white hover:bg-opacity-20 rounded-lg mr-6 md:mr-0 md:mb-6"
+        >
+          <Web className="w-8 h-8 text-white " />
+        </a>
+        <a
+          href="https://instagram.com/frekuensi27_"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative p-1 transition duration-100 hover:bg-white hover:bg-opacity-20 rounded-lg mr-6 md:mr-0 md:mb-6"
+        >
+          <Instagram className="w-8 h-8 text-white " />
+        </a>
+        <a
+          href="https://drive.google.com/drive/folders/1EJvtmEEN89cAYJx6qkt71Hy5A3_xE32Q?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative p-1 transition duration-100 hover:bg-white hover:bg-opacity-20 rounded-lg"
+        >
+          <Archive className="w-8 h-8 text-white " />
+          <div className="w-5 h-5 absolute bg-green-400 animate-ping rounded-full -top-1 -right-1" />
+          <div className="w-3 h-3 absolute bg-green-400 rounded-full -top-0 -right-0" />
+        </a>
       </div>
       <div className="z-20 absolute bottom-0 left-0 w-screen py-3 flex justify-center">
-        <p className="text-white text-xs md:text-sm font-code ">
+        <a
+          href="https://github.com/angkasa27/react-frekuensi27"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white text-xs md:text-sm font-code "
+        >
           Fly high, higher and higher
-        </p>
+        </a>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
@@ -174,15 +213,18 @@ export function Sect1() {
 
 export function Logo({ done }) {
   const [open, setOpen] = useState(false);
-  const [logoClass, setLogoClass] = useState(" opacity-0");
+  const [logoClass, setLogoClass] = useState(" ");
   const [logoText, setLogoText] = useState([" opacity-0", " opacity-0"]);
 
   useEffect(() => {
-    if (done)
-      setTimeout(() => {
-        setLogoClass(" animate-blink");
-      }, 1500);
-  }, [done]);
+    if (open) {
+      setLogoClass(
+        " transform rotate-3 scale-110 bg-white bg-opacity-10 bg-glass"
+      );
+    } else {
+      setLogoClass(" ");
+    }
+  }, [open]);
 
   const openMenu = (v) => {
     if (!v) {
@@ -202,9 +244,13 @@ export function Logo({ done }) {
   const logo = (
     <div
       onClick={() => openMenu(open)}
-      className="transition cursor-pointer duration-500 ease-in-out transform hover:rotate-3 hover:scale-110"
+      className={
+        "transition cursor-pointer duration-500 ease-in-out transform hover:rotate-3 hover:scale-110 hover:bg-white hover:bg-opacity-10 w-56 h-56 flex justify-center items-center rounded-full " +
+        logoClass
+      }
     >
-      <div className={"relative w-20 md:w-40 h-24 md:h-48 " + logoClass}>
+      {/* <div className={"relative w-20 md:w-40 h-24 md:h-48 " + logoClass}> */}
+      <div className="relative w-20 md:w-40 h-24 md:h-48 ">
         <img draggable="false" src={SVG.Base} alt="" className="absolute" />
         <img draggable="false" src={SVG.Bottom1} alt="" className="absolute" />
         <img draggable="false" src={SVG.Bottom2} alt="" className="absolute" />
@@ -217,18 +263,29 @@ export function Logo({ done }) {
   return (
     <div className="flex justify-center items-center z-10">
       <div
+        onClick={() => console.log("tsetse")}
         className={
-          "w-16 h-16 border-2 left-0 border-white rounded-full mr-24 hidden md:block " +
+          "md:mr-24 mr-10 flex flex-col justify-center items-center transition-colors duration-100 hover:bg-white hover:bg-opacity-20 p-3 rounded-lg " +
           logoText[0]
         }
-      ></div>
+      >
+        <Event className="w-12 h-12 text-white" />
+        <p className="text-xs md:text-sm mt-2 text-white font-code">
+          reunion.txt
+        </p>
+      </div>
       {logo}
       <div
         className={
-          "w-16 h-16 border-2 left-0 border-white rounded-full ml-24 hidden md:block " +
+          "md:ml-24 ml-10 flex flex-col justify-center items-center transition-colors duration-100 hover:bg-white hover:bg-opacity-20 p-3 rounded-lg " +
           logoText[1]
         }
-      ></div>
+      >
+        <YearBook className="w-12 h-12 text-white" />
+        <p className="text-xs md:text-sm mt-2 text-white font-code">
+          yearbook.pdf
+        </p>
+      </div>
     </div>
   );
 }
